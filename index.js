df91767aad7f8873c18f18f8f369fab70c5f89ea
Abdeltif Azzizi <73204352+Abdeltif1@users.noreply.github.com>
2021-09-24 11:39:22 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+
+const submit = document.getElementById('submit')
+
+
+
+f = null;
+
+
+
+
+   function dropHandler(ev) {
+    console.log('File(s) dropped');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  
+    if (ev.dataTransfer.items) {
+      // Use DataTransferItemList interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.items.length; i++) {
+        // If dropped items aren't files, reject them
+        if (ev.dataTransfer.items[i].kind === 'file') {
+          var file = ev.dataTransfer.items[i].getAsFile();
+          console.log('... file[' + i + '].name = ' + file.name);
+          upload(file)
+          f = file
+        }
+      }
+    } else {
+      // Use DataTransfer interface to access the file(s)
+      for (var i = 0; i < ev.dataTransfer.files.length; i++) {
+        console.log('... file[' + i + '].name = ' + ev.dataTransfer.files[i].name);
+      }
+    }
+  }
+  
+  
+  function dragOverHandler(ev) {
+    console.log('File(s) in drop zone');
+  
+    // Prevent default behavior (Prevent file from being opened)
+    ev.preventDefault();
+  }
+
+  
+
+
+
+  const upload = (file,theme) => {
+    console.log(file)
+    const fd = new FormData();
+    fd.append('pic', file);
+    // send `POST` request
+    fetch('http://localhost:5551/upload', {
+        method: 'POST',
+        body: fd
+    })
+    .then(res => res.json())
+    .then(json => console.log(json))
+  };
+
+
+submit.onclick = () => {
+    upload(f,theme)
+}
